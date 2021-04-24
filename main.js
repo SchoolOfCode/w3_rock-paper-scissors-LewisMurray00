bc5ce6748ed712a1cc28472edc76c1d633769923
Lewis Murray <lewisdmurray00@gmail.com>
2021-04-24 13:34:22 +0100

    Almost there just score left

--- main.js
+++ main.js
@@ -1,36 +1,31 @@
-//DOM
+//Assigning the buttons to a JavaScript variable
 const computerGuess = document.querySelector(".computerGuess");
 const rockButton = document.querySelector(".rockButton");
 const paperButton = document.querySelector(".paperButton");
 const scissorsButton = document.querySelector(".scissorsButton");
 const guessResult = document.querySelector(".guessResult");
-
+const userName = document.querySelector("#uName");
+const scoreChecker = document.querySelector(".scores");
 //game variables
 let userScore = 0;
 let computerScore = 0;
 
 //Randomly generated word
 
-//let computerChoices = ["rock", "paper", "scissors"];
-//let computerChoice = computerChoices[Math.floor(Math.random()*computerChoices.length)];
-
-//function randomComputerChoice(){
-//    return computerChoices[Math.floor(Math.random()*computerChoices.length)];
-//}
-
-//randomComputerChoice();
-
 function computerPlay(){
     const move = ['rock', 'paper', 'scissors']
     return move[Math.floor(Math.random() * 3)];
 }
 
+//Rock function
 function checkRock(){
     let newGuess = computerPlay();
     if (newGuess === "scissors"){
-        guessResult.innerText = "You win";
+        userScore++;
+        guessResult.innerText =" You win and you now have " + userScore + " points";
     } else if (newGuess === "paper"){
-        guessResult.innerText = "You lose";
+        computerScore++;
+        guessResult.innerText = "You lose and the computer now has " + computerScore + " points";
     } else{
         guessResult.innerText = "It is a tie";
     }
@@ -38,12 +33,15 @@ function checkRock(){
 
 rockButton.addEventListener("click", checkRock);
 
+//Paper function
 function checkPaper(){
     let newGuess = computerPlay();
     if (newGuess === "rock"){
-        guessResult.innerText = "You win";
+        userScore++;
+        guessResult.innerText = "You win and you now have " + userScore + " points";
     } else if (newGuess === "scissors"){
-        guessResult.innerText = "You lose";
+        computerScore++;
+        guessResult.innerText = "You lose and the computer now has " + computerScore + " points";
     } else{
         guessResult.innerText = "It is a tie";
     }
@@ -51,88 +49,19 @@ function checkPaper(){
 
 paperButton.addEventListener("click", checkPaper);
 
+//Scissors function
 function checkScissors(){
     let newGuess = computerPlay();
     if (newGuess === "paper"){
-        guessResult.innerText = "You win";
+        userScore++;
+        guessResult.innerText = "You win and you now have " + userScore + " points";
     } else if (newGuess === "rock"){
-        guessResult.innerText = "You lose";
+        computerScore++;
+        guessResult.innerText = "You lose and the computer now has " + computerScore + " points";
     } else{
         guessResult.innerText = "It is a tie";
     }
 }
 
 scissorsButton.addEventListener("click", checkScissors);
-//User input
-
-//let stillPlaying = true;
-
-//while(stillPlaying === true){
-//
-//    let userChoices = prompt("Rock, Paper or Scissors?");
-//    let userChoice = userChoices.toLowerCase();
-//
-//   while (userChoice !== "rock" && userChoice !== "paper" && userChoice !== "scissors") {
-//        userChoice = prompt("You have to put in Rock,Paper or Scissors...");
-//    }
-//
-//    console.log("You have chosen: " + userChoice);
-//
-//Computer randomly generated input
-//
- //   let computerChoices = ["rock", "paper", "scissors"];
-//    let computerChoice = computerChoices[Math.floor(Math.random()*computerChoices.length)];
-//
-//function for the randomly generated wordBreak: 
-//Working out who won?
-//
-//    var results = function(choice1, choice2) {
-//        if(choice1 == choice2) {
-//        alert("The result is a tie!");
-//    }
-//    
-//    if(choice1 == "rock") {
-//        if(choice2 == "scissors") {
-//            alert("The user wins with rock");
-//            userScore++;
-//        } else {
-//           alert("The computer wins with paper");
-//            computerScore++;
-//        }
-//    }
-//    
-//    else if(choice1 == "paper") {
-//        if(choice2 == "rock") {
-//            alert("The user wins with paper");
-//            userScore++;
-//        } else {
-//            alert("The computer wins with scissors");
- //           computerScore++;
-//        }
-//    }
-
-//    if(choice1 == "scissors") {
-//        if(choice2 == "rock") {
-//            alert("The user wins with scissors");
-//            userScore++;
-//        } else {
-//            alert("The computer wins with rock");
-//            computerScore++;
-//            }
-//        }
-//    };
-    
-//    results(userChoice, computerChoice);
-//    randomComputerChoice();
-//    console.log(
-//        "You have: " + userScore + " points and the computer has: " + computerScore + " points" 
-//    );
-//        let playAgain = prompt("Would you like to play again ? Please type yes or no");
-//        if(playAgain == "yes"){
-//            stillPlaying = true
-//        } else{
- //           stillPlaying = false
-//        }
-            
-//}
 
